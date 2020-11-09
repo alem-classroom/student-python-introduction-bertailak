4e7e83a200626def90fd834351500fab9d15f6fb
bertailak <bertailak@gmail.com>
2020-11-09 23:30:17 +0600

    condition task

--- if-else/conditional.py
+++ if-else/conditional.py
@@ -2,15 +2,16 @@ import random
 
 def is_positive(num):
     # return true if num is positive, otherwise return false
+    return num>=0
 
 def is_even(num):
     # return true if num is even, otherwise return false
-
+    return num%2==0
 
 def is_positive_and_even(num):
     # return true if num is positive and even, otherwise return false
-
+    return num>0 && num%2==0
 
 def is_positive_or_even(num):
     # return true if num is positive or even, otherwise return false
-
+    return num>0 || num%2==0
