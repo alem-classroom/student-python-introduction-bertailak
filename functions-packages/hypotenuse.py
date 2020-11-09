a4a7e29705b2d0e4db13c950515dd7afb52163da
bertailak <bertailak@gmail.com>
2020-11-10 00:01:14 +0600

    'tasks'

--- functions-packages/hypotenuse.py
+++ functions-packages/hypotenuse.py
@@ -1,3 +1,5 @@
 import math   
 def get_hypotenuse(catet1, catet2):  
     # calculate hypotenuse of right triangle based on 2 catets and return it
+    c = catet1 * catet1 + catet2 * catet2   
+    return math.sqrt(c) 
--- loops-for/forDict.py
+++ loops-for/forDict.py
@@ -1,2 +1,6 @@
 def reverse_dict(dict):
     # swap keys and values within dict and return dict
+    dict2 = {}
+    for i in dict.keys():
+        dict2[dict[i]]= i
+    return dict2
