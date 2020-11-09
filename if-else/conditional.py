e8e50fd8695dd99f7426b90d01b64629952c6e5e
bertailak <bertailak@gmail.com>
2020-11-10 00:05:17 +0600

    'tasks2'

--- if-else/conditional.py
+++ if-else/conditional.py
@@ -10,8 +10,9 @@ def is_even(num):
 
 def is_positive_and_even(num):
     # return true if num is positive and even, otherwise return false
-    return num>0 && num%2==0
+    return num>0 and num%2==0
 
 def is_positive_or_even(num):
     # return true if num is positive or even, otherwise return false
-    return num>0 || num%2==0
+    return num>0 or num%2==0
+
